3c17e274b3b45705a3518bddc8e311b747b37751
kali <kali@kali.kali>
2021-12-04 14:07:59 -0500

    Version 1.0.0+6

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({ Key key }) : super(key: key);
+  const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +13,24 @@ class HomePage extends StatelessWidget {
             pinned: true,
             backgroundColor: Colors.white,
             leading: IconButton(
-              icon: Icon(Icons.menu),
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
               onPressed: () {},
             ),
-          )
+          ),
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 30),
+                  child: Text('Browse by Categories'),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
