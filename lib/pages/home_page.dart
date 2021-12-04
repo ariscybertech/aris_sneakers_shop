49f58cbccfc628ee1e51fa047cdde92d91ef7b74
kali <kali@kali.kali>
2021-12-04 14:20:21 -0500

    Version 1.0.0+7

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,10 +24,32 @@ class HomePage extends StatelessWidget {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                SizedBox(height: 10),
                 Padding(
                   padding: const EdgeInsets.only(left: 30),
-                  child: Text('Browse by Categories'),
+                  child: Text(
+                    'Browse by Categories',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
+                SizedBox(height: 40),
+                Container(
+                  color: Colors.amber,
+                  height: MediaQuery.of(context).size.height / 4.15,
+                  child: ListView.builder(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 10,
+                    itemBuilder: (context, index) {
+                    return Container(
+                      color: Colors.black,
+                      width: MediaQuery.of(context).size.width / 1.8,
+                    );
+                  }),
+                )
               ],
             ),
           ),
