21f99fc5f29ce08fd7457ed092fb78eacca05e09
kali <kali@kali.kali>
2021-12-05 05:19:43 -0500

    Version 1.0.0+13

--- lib/widgets/shoe_grid.dart
+++ lib/widgets/shoe_grid.dart
@@ -1,5 +1,6 @@
 import 'package:aris_sneakers_shop/models/shoe.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ShoeGrid extends StatelessWidget {
   const ShoeGrid({Key key, this.shoes}) : super(key: key);
@@ -15,19 +16,63 @@ class ShoeGrid extends StatelessWidget {
         ),
         delegate: SliverChildBuilderDelegate(
           (context, index) {
-            return Container(
-              decoration: BoxDecoration(
-                color: shoes[index].bgColor.withOpacity(0.75),
-                borderRadius: BorderRadius.circular(35),
-              ),
-              child: Column(
-                children: [
-                  Stack(
-                    children: [
-                      Image.asset('${shoes[index].imageName}'),
-                    ],
-                  )
-                ],
+            return Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: shoes[index].bgColor.withOpacity(0.75),
+                  borderRadius: BorderRadius.circular(35),
+                ),
+                child: Column(
+                  children: [
+                    Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Image.asset(
+                          '${shoes[index].imageName}',
+                          height: 150,
+                        ),
+                        Container(
+                          height: 50,
+                          width: MediaQuery.of(context).size.width / 4,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.25),
+                                spreadRadius: 10,
+                                blurRadius: 30,
+                                offset: Offset(0, 10),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Spacer(),
+                    Text(
+                      shoes[index].name,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      shoes[index].price,
+                      style: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    )
+                  ],
+                ),
               ),
             );
           },
