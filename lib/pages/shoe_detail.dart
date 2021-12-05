3ef1c96570d6c8298941c464fa6c57814febc5ce
kali <kali@kali.kali>
2021-12-05 08:04:47 -0500

    Version 1.0.0+18

--- lib/pages/shoe_detail.dart
+++ lib/pages/shoe_detail.dart
@@ -1,5 +1,9 @@
 import 'package:aris_sneakers_shop/models/shoe.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class ShoeDetail extends StatelessWidget {
   const ShoeDetail({Key key, this.shoe}) : super(key: key);
@@ -7,34 +11,223 @@ class ShoeDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).primaryColor,
-          elevation: 0,
-          actions: [
-            Padding(
-              padding: const EdgeInsets.only(right: 10),
-              child: Stack(
+        elevation: 0,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Stack(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.shopping_bag_outlined,
+                    color: Colors.black,
+                    size: 30,
+                  ),
+                ),
+                Positioned(
+                  right: 6,
+                  bottom: 10,
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 18,
+                    height: 18,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF5E68E8),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      '1',
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(Icons.arrow_back),
+          color: Colors.black,
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Theme.of(context).primaryColor,
+              padding: EdgeInsets.only(left: 40),
+              child: Row(
                 children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.shopping_bag_outlined,
-                      color: Colors.black,
-                      size: 30,
-                    ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'LIMITED',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        shoe.name,
+                        style: TextStyle(
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'From',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        shoe.price,
+                        style: TextStyle(
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Text(
+                        'Available Colors',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          ...shoe.availableColors.map(
+                            (e) => Container(
+                              margin: EdgeInsets.only(right: 15),
+                              decoration: BoxDecoration(
+                                color: e,
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              height: 30,
+                              width: 30,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
-                  Positioned(
-                    child: Container(
-                      alignment: Alignment.center,
-                      width: 18,
-                      height: 18,
+                  Expanded(
+                    child: Image.asset(
+                      '${shoe.imageName}',
+                      fit: BoxFit.cover,
+                      alignment: Alignment.centerLeft,
                     ),
-                  ),
+                  )
                 ],
               ),
-            )
-          ],
-        ));
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(40, 60, 40, 60),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Description',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 25),
+                  Text(
+                    shoe.description,
+                    style: TextStyle(
+                      height: 1.5,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2.6,
+                        height: 60,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFF5E68E8),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          'Buy Now',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2.6,
+                        height: 60,
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Color(0xFFF5E68E8),
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Text(
+                          'Buy Now',
+                          style: TextStyle(
+                            color: Color(0xFFF5E68E8),
+                            fontSize: 16,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
